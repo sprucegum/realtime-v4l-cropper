8eaa405c315ab90e12752c43575bc5f26eadd497
Jade Lacosse <jade.lacosse@stocksy.com>
2019-11-04 01:14:47 -0800

    removed sleep and replaced it with a time delta calculation

--- webcam-rt.py
+++ webcam-rt.py
@@ -40,77 +40,79 @@ def run_rt(webcam):
     next_update_val = [0, 0, 0, 0]
     current_viewport = [0,0,0,0]
     first_frame = True
+    next_frame = now + timedelta(seconds = 1/30.0)
     while True:
-        frame_count = (frame_count + 1) % 30
-        ret, frame = video_capture.read()
-        frame = cv2.resize(frame, (640, 360))
-        # flip image, because webcam inverts it and we trained the model the other way!
-        frame = cv2.flip(frame,1)
-        image = frame
-        # flip it back
-        image = cv2.flip(image,1)
         now = datetime.now()
-        try:
-            faces_rects = haar_cascade_face.detectMultiScale(image, scaleFactor = 1.2, minNeighbors = 5)
-            for i in range(4):
-                if next_update_time[i] < now:
-                    current_viewport[i] += next_update_val[i]
-                    next_update_val[i] = 0
-            if len(faces_rects) == 1:
-                if first_frame:
-                    current_viewport = np.copy(faces_rects[0])
-                    first_frame = False
+        if (now > next_frame):
+            next_frame = now + timedelta(seconds = 1/30.0)
+            frame_count = (frame_count + 1) % 30
+            ret, frame = video_capture.read()
+            frame = cv2.resize(frame, (640, 360))
+            # flip image, because webcam inverts it and we trained the model the other way!
+            frame = cv2.flip(frame,1)
+            image = frame
+            # flip it back
+            image = cv2.flip(image,1)
+            try:
+                faces_rects = haar_cascade_face.detectMultiScale(image, scaleFactor = 1.2, minNeighbors = 5)
                 for i in range(4):
-                    diff = current_viewport[i] - faces_rects[0][i]
-                    next_update_val[i] = 0
-                    if diff > 5:
-                        next_update_val[i] = -1
-                    if diff < -5:
-                        next_update_val[i] = 1
-                    if abs(diff) > 5:
-                        next_update_time[i] = now + (td/(max(abs(diff), 90)))
+                    if next_update_time[i] < now:
+                        current_viewport[i] += next_update_val[i]
+                        next_update_val[i] = 0
+                if len(faces_rects) == 1:
+                    if first_frame:
+                        current_viewport = np.copy(faces_rects[0])
+                        first_frame = False
+                    for i in range(4):
+                        diff = current_viewport[i] - faces_rects[0][i]
+                        next_update_val[i] = 0
+                        if diff > 5:
+                            next_update_val[i] = -1
+                        if diff < -5:
+                            next_update_val[i] = 1
+                        if abs(diff) > 5:
+                            next_update_time[i] = now + (td/(max(abs(diff), 60)))
 
-            current_x, current_y, current_h, current_w = current_viewport
-            image = image[current_y-eh2:current_y+current_h+eh2, current_x-ew2:current_x+current_w+ew2]
-            image_height = current_h + extra_height
-            image_width = current_w + extra_width
-            #scale_ratio = image_width/(image_height*1.0)
-            target_width = int((360.0/image_height)*image_width)
-            image = cv2.resize(image, (target_width, 360))
-            blank_image = np.zeros((360, 640, 3), np.uint8)
-            paste_position = int((640/2) - image_width/2)
-            blank_image[0:360, paste_position:paste_position + target_width] = image
-            image = blank_image
-            #image = cv2.resize(image, (640, 360))
-            #print(faces_rects)
+                current_x, current_y, current_h, current_w = current_viewport
+                image = image[current_y-eh2:current_y+current_h+eh2, current_x-ew2:current_x+current_w+ew2]
+                image_height = current_h + extra_height
+                image_width = current_w + extra_width
+                #scale_ratio = image_width/(image_height*1.0)
+                target_width = int((360.0/image_height)*image_width)
+                image = cv2.resize(image, (target_width, 360))
+                blank_image = np.zeros((360, 640, 3), np.uint8)
+                paste_position = int((640/2) - image_width/2)
+                blank_image[0:360, paste_position:paste_position + target_width] = image
+                image = blank_image
+                #image = cv2.resize(image, (640, 360))
+                #print(faces_rects)
 
 
-            if (webcam):
-                time.sleep(1/30.0)
-                # firefox needs RGB
+                if (webcam):
+                    # firefox needs RGB
 
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                # chrome and skype UYUV - not working at the
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    # chrome and skype UYUV - not working at the
 
-                # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                # chrome and skype UYUV - not working at the moment
+                    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    # chrome and skype UYUV - not working at the moment
 
-                # image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+                    # image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
 
-                camera.schedule_frame(image)
-                #print("writing to stream")
+                    camera.schedule_frame(image)
+                    #print("writing to stream")
 
-            else:
-                cv2.imshow('Video', image)
-                #print("writing to screen")
-        except Exception as e:
-            print(e)
-            pass
+                else:
+                    cv2.imshow('Video', image)
+                    #print("writing to screen")
+            except Exception as e:
+                print(e)
+                pass
 
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            video_capture.release()
-            break
+            # Hit 'q' on the keyboard to quit!
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                video_capture.release()
+                break
 
 if __name__ == "__main__":
     run_rt(True)
