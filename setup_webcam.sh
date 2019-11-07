8f1605a2e9b7ac7449322d350080f6583147ca23
Jade Lacosse <jade.lacosse@stocksy.com>
2019-11-07 00:21:21 -0800

    Enable exclusive caps which allows chrome to see the device

--- setup_webcam.sh
+++ setup_webcam.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo modprobe -r uvcvideo && sudo modprobe uvcvideo quirks=128
-sudo modprobe v4l2loopback video_nr=1
+sudo modprobe v4l2loopback video_nr=1 exclusive_caps=1
 v4l2-ctl --list-devices
